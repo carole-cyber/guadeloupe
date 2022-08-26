e8ede56ce6ea538c6755a23ad1658aadf0d1e7d5
carole <castardjulie@gmail.com>
2022-08-26 19:26:57 -0400

    mon premier commit

---
+++ main.js
@@ -0,0 +1,15 @@
+//creation de l'animation en javascript
+
+
+const swiper = new Swiper('.swiper', {
+
+    direction: 'horizontal',
+    loop: true,
+  
+// les fleches
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
